f29fe542a64b7e2d95cde1ce6f8685ccd0284ae1
Mykhailo Filonych <mihailfilonich@gmail.com>
2023-04-07 14:15:16 +0300

    part-4 added link to wrapper

--- twentytwenty-child/template-parts/product-card.php
+++ twentytwenty-child/template-parts/product-card.php
@@ -5,9 +5,11 @@ if($args['id']){
 
 $sale_price = get_post_meta( $id, 'custom_metabox_value_sale_price', true );
 $thumbnail = get_the_post_thumbnail( $id, 'large', array( 'class' => 'post-thumbnail' ) );
+$url = get_permalink($id);
 
 $html_output = '';
 $html_output .= '<div class="el-product-item">';
+$html_output .= '<a href="'. $url .'">';
 
 $html_output .= $thumbnail;
 $html_output .= '<h2>' . get_the_title($id) . '</h2>';
@@ -16,6 +18,8 @@ $html_output .= '<p>' . $price_html . '</p>';
 if(!empty($sale_price)){
     $html_output .= '<h5 class="el-product-item_sale-badge">Product on Sale!</h5>';
 }
+$html_output .= '</a>';
 $html_output .= '</div>';
+
 
 echo $html_output;
