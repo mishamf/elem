284dfa33d96febecaf415f9477ea8581b8457a8a
Mykhailo Filonych <mihailfilonich@gmail.com>
2023-04-07 13:19:15 +0300

    part-5 shortcode for product

---
+++ twentytwenty-child/app/shortcodes/product.php
@@ -0,0 +1,42 @@
+<?php
+function product_shortcode( $atts ) {
+    // Set default attributes
+    $atts = shortcode_atts( array(
+      'id' => '',
+      'bg_color' => ''
+    ), $atts );
+  
+    if(empty($atts['id'])){
+        return '';
+    }
+    $id = filter_var($atts['id'], FILTER_SANITIZE_NUMBER_INT);
+    $thumbnail = get_the_post_thumbnail( $id, 'large', array( 'class' => 'post-thumbnail' ) );
+
+  
+    // Get box background color
+    $bg_color = filter_var($atts['bg_color'], FILTER_SANITIZE_STRING);
+    if(!empty($bg_color)){
+        $bg_color = 'style="background-color:' . $bg_color . '"';
+    }
+
+    $price = get_post_meta( $id, 'custom_metabox_value_price', true );
+    $sale_price = get_post_meta( $id, 'custom_metabox_value_sale_price', true );
+
+    $price_html = '<b>' . $price . '</b>'; 
+
+    if(!empty($sale_price)){
+        $price_html = '<strike>' . $price . '</strike><br>';
+        $price_html .= '<b>' . $sale_price . '</b>'; 
+    }
+
+  
+    // Output product box
+    $output = '<div ' . $bg_color . '>';
+    $output .= $thumbnail;
+    $output .= '<h2>' . get_the_title($id) . '</h2>';
+    $output .= '<p>' . $price_html . '</p>';
+    $output .= '</div>';
+  
+    return $output;
+  }
+  add_shortcode( 'product_box', 'product_shortcode' );
